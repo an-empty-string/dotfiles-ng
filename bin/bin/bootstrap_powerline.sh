e471999abf3031e8e26f40cbdb947f997e061507
Fox Wilson <foxwilson123@gmail.com>
2016-12-08 20:38:41 -0600

    fix typo

--- bin/bin/bootstrap_powerline.sh
+++ bin/bin/bootstrap_powerline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.config/fontconfig/conf.d
 curl -L -o ~/.fonts/PowerlineSymbols.otf https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-curl -L -o ~/.config/fontconfig/conf.d https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+curl -L -o ~/.config/fontconfig/conf.d/10-powerline-symbols.conf https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 fc-cache -fv ~/.fonts
