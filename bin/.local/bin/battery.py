cf4d238c704016b9a418e18df6b5d7f4196eafe4
Fox Wilson <foxwilson123@gmail.com>
2016-12-11 04:21:28 -0500

    update battery.py

--- bin/.local/bin/battery.py
+++ bin/.local/bin/battery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 base = "/sys/class/power_supply/BAT0/"
 import colorsys
+import sys
 
 def pct():
     return int(open(base + "charge_now").read().strip()) / int(open(base + "charge_full").read().strip())
@@ -14,7 +15,11 @@ if p > 100:
     p = 100
 
 dt = "+" if c else "-"
-print("{}%{}".format(p, dt))
-print(p)
-r, g, b = colorsys.hsv_to_rgb((p * 1.2) / 360, 0.99, 0.99)
-print("#%02x%02x%02x" % (int(r*256), int(g*256), int(b*256)))
+if len(sys.argv) < 2 or sys.argv[1] != "prompt":
+    print("{}%{}".format(p, dt))
+    print(p)
+    r, g, b = colorsys.hsv_to_rgb((p * 1.2) / 360, 0.99, 0.99)
+    print("#%02x%02x%02x" % (int(r*256), int(g*256), int(b*256)))
+else:
+    color = "red" if p < 20 else "yellow" if p < 40 else "green"
+    print("%F{{{}}}{}%%{}%f".format(color, p, dt))
